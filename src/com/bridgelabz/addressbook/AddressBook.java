97b1a75b5349d5b90320646ae64deb082f86c31c
adarshEr2020 <adarshprajapati31@gmail.com>
2021-06-26 09:52:25 +0530

    Added: add new contact

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -6,35 +6,47 @@ import java.util.Scanner;
 
 public class AddressBook {
 	static Scanner sc = new Scanner(System.in);
-	static List<Contact> list = new LinkedList<Contact>();
+    static List<Contact> list = new LinkedList<Contact>();
 
-	//Created method for adding contact
-	public static void addContact()
-	{
-		System.out.println("Enter your firstName : ");
-		String firstName = sc.nextLine();
-		System.out.println("Enter your lastName : ");
-		String lastName = sc.nextLine();
-		System.out.println("Enter your address : ");
-		String address = sc.nextLine();
-		System.out.println("Enter your city : ");
-		String city = sc.nextLine();
-		System.out.println("Enter your state : ");
-		String state = sc.nextLine();
-		System.out.println("Enter your pin : ");
-		String pin = sc.nextLine();
-		System.out.println("Enter your MobileNo : ");
-		String MobileNo = sc.nextLine();
-		System.out.println("Enter your email : ");
-		String email = sc.nextLine();
-		
-		Contact obj = new Contact(firstName, lastName, address, city, state, pin, MobileNo, email);
-		list.add(obj);
-	}
+    //Created method for adding contact
+    public static void addContact()
+    {
+        System.out.println("Enter your firstName : ");
+        String firstName = sc.nextLine();
+        System.out.println("Enter your lastName : ");
+        String lastName = sc.nextLine();
+        System.out.println("Enter your address : ");
+        String address = sc.nextLine();
+        System.out.println("Enter your city : ");
+        String city = sc.nextLine();
+        System.out.println("Enter your state : ");
+        String state = sc.nextLine();
+        System.out.println("Enter your pin : ");
+        String pin = sc.nextLine();
+        System.out.println("Enter your MobileNo : ");
+        String MobileNo = sc.nextLine();
+        System.out.println("Enter your email : ");
+        String email = sc.nextLine();
+        Contact obj = new Contact(firstName, lastName, city, state, pin, MobileNo, email);
+        list.add(obj);
+    }
 
-	public static void main(String[] args) {
-		System.out.println("***Welcome To Address Book System*** ");
+
+public static void main(String[] args) {
+
+	AddressBook addressBook = new AddressBook();
+	//Displaying the welcome message
+	System.out.println("WELCOME TO ADDRESS BOOK PROBLEM");
+	//Adding new contact
+	System.out.println("Enter details of new contact");
+	addContact();
+
+	//Creating contact and adding new contact details to the list
+	System.out.println("You want to add contact M/N");
+	String chose = sc.nextLine();
+	if(chose.equalsIgnoreCase("y"))
 		addContact();
-        System.out.println(list);
-	}
+	else
+		System.out.println(list); //printing list
+}
 }
